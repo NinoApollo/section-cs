9606a7186a628394698aec0fb3922709ab88b66a
Niño Apollo <napollo@filamer.edu.ph>
2026-04-03 16:32:22 +0800

    Added header and sidebar and its functions

---
+++ client/src/layout/AppLayout.tsx
@@ -0,0 +1,35 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "./AppHeader";
+import AppSidebar from "./AppSidebar";
+import { SidebarProvider } from "../context/SidebarContext";
+import { HeaderProvider } from "../context/HeaderContext";
+
+const LayoutContent = () => {
+  return (
+    <>
+      <div>
+        <AppSidebar />
+      </div>
+      <div>
+        <AppHeader />
+      </div>
+      <div className="p-20 -ml-14 sm:ml-52">
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderProvider>
+        <SidebarProvider>
+          <LayoutContent />
+        </SidebarProvider>
+      </HeaderProvider>
+    </>
+  );
+};
+
+export default AppLayout;
