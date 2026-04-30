5e98f62295316a654eb2691ff2dc3dcaee66d32b
Kristianity23 <https://github.com/Kristianity23>
2026-04-30 09:28:32 +0800

    apollo-flores-stevens

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\GenderController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+   
+
+Route::controller(AuthController::class)->prefix('/auth')->group(function() {
+    Route::post('/login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(AuthController::class)->prefix('/auth')->group(function() {
+    Route::get('/me', 'me');
+    Route::post('/logout', 'logout');
+});
 
 Route::controller(GenderController::class)->prefix('/gender')->group(function() {
     Route::get('/loadGenders', 'loadGenders'); // /gender/loadGenders
@@ -19,6 +31,9 @@ Route::controller(UserController::class)->prefix('/user')->group(function() {
     Route::put('/updateUser/{user}', 'updateUser');
     Route::put('/destroyUser/{user}', 'destroyUser');
 });
+});
+
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
